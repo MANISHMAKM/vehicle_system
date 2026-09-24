df61c20ba2ecbe3c2f7c6c79cff7566628ac41c1
MANISHMAKM <manishmakmanohar@gmail.com>
2026-09-24 16:44:16 +0530

    url

--- vehicle_system/urls.py
+++ vehicle_system/urls.py
@@ -5,7 +5,7 @@ from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 urlpatterns = [
     path("admin/", admin.site.urls),
 
-    path("api/", include("inventory.urls")),
+    path("", include("inventory.urls")),
 
     path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
     path(
